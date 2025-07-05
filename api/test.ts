793ce05da3c5bc0894ea57f4027cb4daeb30af18
gajindubandara <gajindukb@gmail.com>
2025-07-05 10:37:28 +0530

    Update

--- api/test.ts
+++ api/test.ts
@@ -1,10 +1,20 @@
+// import express from "express";
+// import serverless from "serverless-http";
+// import { getTest } from "./controllers/testController.js";
+//
+// const app = express();
+//
+// // This will match any request path
+// app.use(getTest);
+//
+// export default serverless(app);
 import express from "express";
 import serverless from "serverless-http";
-import { getTest } from "./controllers/testController.js";
 
 const app = express();
 
-// This will match any request path
-app.use(getTest);
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from minimal test API" });
+});
 
 export default serverless(app);
