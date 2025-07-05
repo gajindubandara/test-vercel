1a5124987c3c7991aa63b78f40833e13da919936
gajindubandara <gajindukb@gmail.com>
2025-07-05 10:35:40 +0530

    Update

--- api/test.ts
+++ api/test.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import serverless from "serverless-http";
-import {getTest} from "./controllers/testController.js";
+import { getTest } from "./controllers/testController.js";
 
 const app = express();
 
-app.get("/", getTest);
+// This will match any request path
+app.use(getTest);
 
 export default serverless(app);
