8326d6f3d2447313faf2871a91fa823468026254
gajindubandara <gajindukb@gmail.com>
2025-07-05 15:25:54 +0530

    New update

--- api/controllers/testController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export const getTest = (req: Request, res: Response) => {
-    res.json({
-        message: "Hello from the test API endpoint (TypeScript)!"
-    });
-};
